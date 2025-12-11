b886f8653936a3efa451842999443758565faa31
ahmad292005 <ahmadfathalla581@gmail.com>
2025-12-11 02:53:38 +0200

    push

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
-}
-git status
-git remote -v
-=======
 import com.sun.opengl.util.Animator;
 import javax.media.opengl.GLCanvas;
 
@@ -65,4 +48,4 @@ class Game {
         animator.start();
     }
 }
->>>>>>> 394f5e75b3d8c64a14e6abb1594baf9478c1be7a
+
